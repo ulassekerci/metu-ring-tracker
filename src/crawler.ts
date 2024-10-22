7248e934cea7e6284cf403b10ee6a2606f7a221f
ulassekerci <107992457+ulassekerci@users.noreply.github.com>
2024-10-22 10:22:26 +0300

    Crawl function try catch block

--- src/crawler.ts
+++ src/crawler.ts
@@ -12,46 +12,50 @@ export const lastCrawl = {
 }
 
 export const crawl = async () => {
-  const ringReq = await axios.get('https://ring.metu.edu.tr/ring.json')
-  const ringData = ringReq.data as RingData[] | string
-  lastCrawl.timestamp = DateTime.now().setZone('Europe/Istanbul')
+  try {
+    const ringReq = await axios.get('https://ring.metu.edu.tr/ring.json')
+    const ringData = ringReq.data as RingData[] | string
+    lastCrawl.timestamp = DateTime.now().setZone('Europe/Istanbul')
 
-  // If there is no data, return
-  if (typeof ringData === 'string') {
-    lastCrawl.data = null
-    lastCrawl.vehicles = []
-    return
-  }
-
-  // If there is no movement, return
-  if (!checkMovement(ringData)) return
-
-  // Update lastCrawl
-  lastCrawl.data = ringData
-  lastCrawl.vehicles.map((oldVehicle) => {
-    if (ringData.find((v) => v.id === oldVehicle.plate)) return
-    lastCrawl.vehicles.splice(
-      lastCrawl.vehicles.findIndex((v) => v.plate === oldVehicle.plate),
-      1
-    )
-  })
-
-  // Record to database
-  ringData.map(async (ring) => {
-    const lastVehicle = lastCrawl.vehicles.find((v) => v.plate === ring.id)
-    const vehicle = lastVehicle || { tripID: nanoid(), plate: ring.id }
-    lastCrawl.vehicles.push(vehicle)
-
-    const historyData = {
-      trip_id: vehicle?.tripID || nanoid(),
-      lat: ring.lat,
-      lng: ring.lng,
-      address: ring.addr,
-      color: ring.clr,
-      state: ring.key,
-      plate: ring.id,
+    // If there is no data, return
+    if (typeof ringData === 'string') {
+      lastCrawl.data = null
+      lastCrawl.vehicles = []
+      return
     }
 
-    await sql`INSERT INTO ring_history ${sql(historyData)}`
-  })
+    // If there is no movement, return
+    if (!checkMovement(ringData)) return
+
+    // Update lastCrawl
+    lastCrawl.data = ringData
+    lastCrawl.vehicles.map((oldVehicle) => {
+      if (ringData.find((v) => v.id === oldVehicle.plate)) return
+      lastCrawl.vehicles.splice(
+        lastCrawl.vehicles.findIndex((v) => v.plate === oldVehicle.plate),
+        1
+      )
+    })
+
+    // Record to database
+    ringData.map(async (ring) => {
+      const lastVehicle = lastCrawl.vehicles.find((v) => v.plate === ring.id)
+      const vehicle = lastVehicle || { tripID: nanoid(), plate: ring.id }
+      lastCrawl.vehicles.push(vehicle)
+
+      const historyData = {
+        trip_id: vehicle?.tripID || nanoid(),
+        lat: ring.lat,
+        lng: ring.lng,
+        address: ring.addr,
+        color: ring.clr,
+        state: ring.key,
+        plate: ring.id,
+      }
+
+      await sql`INSERT INTO ring_history ${sql(historyData)}`
+    })
+  } catch (error) {
+    console.error(error)
+  }
 }
