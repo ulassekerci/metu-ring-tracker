6a946697f88555c91ec41ec5dc3f675b3664601b
ulassekerci <107992457+ulassekerci@users.noreply.github.com>
2024-11-27 11:44:56 +0300

    Implemented live data fetching

--- src/app/routes/home.tsx
+++ src/app/routes/home.tsx
@@ -1,4 +1,4 @@
-import { AvgPoint, fetchAverages } from '@/features/live/api'
+import { AvgPoint, fetchAverages, fetchLive, LivePoint } from '@/features/live/api'
 import { getBetterColor } from '@/lib/colors'
 import { useQuery } from '@tanstack/react-query'
 import { DateTime } from 'luxon'
@@ -6,21 +6,34 @@ import { useEffect, useState } from 'react'
 import Map, { Marker } from 'react-map-gl/maplibre'
 
 export default function Home() {
-  const [ringPoints, setRingPoints] = useState<AvgPoint[]>([])
+  const [ghostPoints, setGhostPoints] = useState<AvgPoint[]>([])
+  const [livePoints, setLivePoints] = useState<LivePoint[]>([])
 
-  const { data } = useQuery({
-    queryKey: ['liveData'],
+  const { data: avgData } = useQuery({
+    queryKey: ['avgdata'],
     queryFn: () => fetchAverages(),
     refetchOnMount: false,
     refetchOnWindowFocus: false,
   })
 
-  const updatePins = () => {
-    if (!data) return
-    const newRingPoints: AvgPoint[] = []
-    const departures = new Set(data.map((point) => point.departure))
+  const { data: liveData } = useQuery({
+    queryKey: ['livedata'],
+    queryFn: () => fetchLive(),
+    refetchInterval: 1000,
+  })
+
+  const updateBuses = () => {
+    if (!liveData?.data) return
+    const newLivePoints = liveData.data
+    setLivePoints(newLivePoints)
+  }
+
+  const updateGhosts = () => {
+    if (!avgData) return
+    const newGhostPoints: AvgPoint[] = []
+    const departures = new Set(avgData.map((point) => point.departure))
     departures.forEach((departure) => {
-      const points = data.filter((point) => point.departure === departure)
+      const points = avgData.filter((point) => point.departure === departure)
       // find closest point in time to now
       const closestPoint = points.reduce((prev, current) => {
         const prevTime = DateTime.fromISO(prev.time, { zone: 'Europe/Istanbul' })
@@ -29,16 +42,20 @@ export default function Home() {
         const currentDiff = Math.abs(currentTime.diffNow('seconds').seconds)
         return prevDiff < currentDiff ? prev : current
       })
-      newRingPoints.push(closestPoint)
+      newGhostPoints.push(closestPoint)
     })
-    setRingPoints(newRingPoints)
+    setGhostPoints(newGhostPoints)
   }
 
   useEffect(() => {
-    updatePins()
-    const updateInterval = setInterval(updatePins, 1000)
+    updateBuses()
+  }, [liveData?.timestamp])
+
+  useEffect(() => {
+    updateGhosts()
+    const updateInterval = setInterval(updateGhosts, 1000)
     return () => clearInterval(updateInterval)
-  }, [data])
+  }, [avgData])
 
   return (
     <>
@@ -52,10 +69,14 @@ export default function Home() {
         mapStyle='/mapstyle.json'
         attributionControl={false}
       >
-        {ringPoints.map((point) => (
+        {ghostPoints.map((point) => (
           <Marker key={point.id} longitude={Number(point.lng)} latitude={Number(point.lat)}>
-            {/* <img width={24} height={24} src='./favicon.ico' /> */}
             <GhostIcon color={point.color} />
+          </Marker>
+        ))}
+        {livePoints.map((point) => (
+          <Marker key={point.id} longitude={Number(point.lng)} latitude={Number(point.lat)}>
+            <img width={24} height={24} src='./favicon.ico' />
           </Marker>
         ))}
       </Map>
@@ -68,9 +89,13 @@ const GhostIcon = ({ color }: { color: string }) => {
     <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='none' viewBox='0 0 24 24'>
       <path
         fill={getBetterColor(color)}
+        stroke='#ffffff'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth='1'
         d='M12 2a8 8 0 0 0-8 8v12l3-3 2.5 2.5L12 19l2.5 2.5L17 19l3 3V10a8 8 0 0 0-8-8'
       ></path>
-      <path stroke='#fff' strokeLinecap='round' strokeLinejoin='round' strokeWidth='3' d='M9 10h.01M15 10h.01'></path>
+      <path stroke='#fff' strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M9 10h.01M15 10h.01'></path>
     </svg>
   )
 }
--- src/features/live/api.ts
+++ src/features/live/api.ts
@@ -10,7 +10,29 @@ export interface AvgPoint {
   time: string
 }
 
+export interface LiveData {
+  data: LivePoint[]
+  timestamp: string
+}
+
+export interface LivePoint {
+  lat: string
+  lng: string
+  addr: string
+  dir: number
+  sp: string
+  clr: string
+  ago: number
+  key: string
+  id: string
+}
+
 export const fetchAverages = async () => {
   const response = await axios.get(`${import.meta.env.VITE_API_URL}/averages`)
   return response.data as AvgPoint[]
+}
+
+export const fetchLive = async () => {
+  const response = await axios.get(`${import.meta.env.VITE_API_URL}`)
+  return response.data as LiveData
 }
