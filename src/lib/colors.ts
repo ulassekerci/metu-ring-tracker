93dbad30df702fbd6dffd20615e044d225d90e82
ulassekerci <107992457+ulassekerci@users.noreply.github.com>
2024-11-27 11:44:35 +0300

    Changed yellow color to a more visible one

--- src/lib/colors.ts
+++ src/lib/colors.ts
@@ -8,7 +8,7 @@ export const getBetterColor = (hex: string) => {
 
 const colorDict = [
   { color: 'red', hex: '#ff0000', better: '#ff4d4d' },
-  { color: 'yellow', hex: '#ffff57', better: '#ffff80' },
+  { color: 'yellow', hex: '#ffff57', better: '#ffcc00' },
   { color: 'purple', hex: '#9600CD', better: '#b366ff' },
   { color: 'brown', hex: '#A64D00', better: '#d97706' },
   { color: 'gray', hex: '#808080', better: '#808080' }, // not sure about this one
