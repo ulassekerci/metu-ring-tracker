81b9858151c4c29155ad676b6dc15c4b9430d864
ulassekerci <107992457+ulassekerci@users.noreply.github.com>
2025-01-16 02:59:38 +0300

    Removed ghost location calculations (moved to backend)

--- src/app/routes/home.tsx
+++ src/app/routes/home.tsx
@@ -1,34 +1,23 @@
 import { Ghost } from '@/features/pins/components/Ghost'
 import { BusPin } from '@/features/pins/components/Pin'
-import { AvgPoint, getGhostLocations, useAverageData } from '@/features/pins/data/average'
+import { useAverageData } from '@/features/pins/data/average'
 import { useLiveData } from '@/features/pins/data/live'
 import { CircleHelpIcon, MenuIcon, SearchIcon } from 'lucide-react'
-import { useEffect, useState } from 'react'
 import Map, { Marker } from 'react-map-gl/maplibre'
 
 export default function Home() {
-  const [ghostPoints, setGhostPoints] = useState<AvgPoint[]>([])
-
-  const { data: avgData } = useAverageData()
+  const { data: ghostData } = useAverageData()
   const { data: liveData } = useLiveData()
-
-  const updateGhosts = () => {
-    if (!avgData) return
-    const newGhostPoints = getGhostLocations(avgData, liveData)
-    setGhostPoints(newGhostPoints)
-  }
-
-  useEffect(() => {
-    updateGhosts()
-    const updateInterval = setInterval(updateGhosts, 1000)
-    return () => clearInterval(updateInterval)
-  }, [avgData])
 
   return (
     <>
       <div className='absolute top-0 w-full z-10 h-14 flex justify-between items-center px-4 text-primary'>
-        <MenuIcon className='w-6 h-6' />
-        <CircleHelpIcon className='w-6 h-6' />
+        <div className='flex items-center bg-white bg-opacity-80 rounded-full p-2 shadow-lg'>
+          <MenuIcon className='w-6 h-6' />
+        </div>
+        <div className='flex items-center bg-white bg-opacity-80 rounded-full p-2 shadow-lg'>
+          <CircleHelpIcon className='w-6 h-6' />
+        </div>
       </div>
       <Map
         initialViewState={{
@@ -45,7 +34,7 @@ export default function Home() {
           [32.873, 39.936],
         ]}
       >
-        {ghostPoints.map((point) => (
+        {ghostData?.map((point) => (
           <Marker key={point.id} longitude={Number(point.lng)} latitude={Number(point.lat)}>
             <Ghost color={point.color} />
           </Marker>
--- src/features/pins/data/average.ts
+++ src/features/pins/data/average.ts
@@ -1,7 +1,5 @@
 import { api } from '@/lib/queryClient'
 import { useQuery } from '@tanstack/react-query'
-import { DateTime } from 'luxon'
-import { LiveData } from './live'
 
 export interface AvgPoint {
   id: number
@@ -22,27 +20,6 @@ export const useAverageData = () => {
   return useQuery({
     queryKey: ['avgdata'],
     queryFn: () => fetchAverages(),
-    refetchOnMount: false,
-    refetchOnWindowFocus: false,
+    refetchInterval: 1000,
   })
-}
-
-export const getGhostLocations = (avgData: AvgPoint[], liveData?: LiveData) => {
-  const ghostPoints: AvgPoint[] = []
-  const ghostDepartures = new Set(avgData.map((point) => point.departure))
-  ghostDepartures.forEach((departure) => {
-    const liveBus = liveData?.vehicles.find((bus) => bus.departure === departure)
-    if (liveBus) return
-    const points = avgData.filter((point) => point.departure === departure)
-    // find closest point in time to now
-    const closestPoint = points.reduce((prev, current) => {
-      const prevTime = DateTime.fromISO(prev.time, { zone: 'Europe/Istanbul' })
-      const currentTime = DateTime.fromISO(current.time, { zone: 'Europe/Istanbul' })
-      const prevDiff = Math.abs(prevTime.diffNow('seconds').seconds)
-      const currentDiff = Math.abs(currentTime.diffNow('seconds').seconds)
-      return prevDiff < currentDiff ? prev : current
-    })
-    ghostPoints.push(closestPoint)
-  })
-  return ghostPoints
 }
