d93ece2c141751ad1cfd8533b5477461358adcd2
ulassekerci <107992457+ulassekerci@users.noreply.github.com>
2025-01-15 17:58:37 +0300

    Created (non-functional) menu and stop data components on home page

--- src/app/routes/home.tsx
+++ src/app/routes/home.tsx
@@ -2,6 +2,7 @@ import { Ghost } from '@/features/pins/components/Ghost'
 import { BusPin } from '@/features/pins/components/Pin'
 import { AvgPoint, getGhostLocations, useAverageData } from '@/features/pins/data/average'
 import { useLiveData } from '@/features/pins/data/live'
+import { CircleHelpIcon, MenuIcon, SearchIcon } from 'lucide-react'
 import { useEffect, useState } from 'react'
 import Map, { Marker } from 'react-map-gl/maplibre'
 
@@ -25,10 +26,14 @@ export default function Home() {
 
   return (
     <>
+      <div className='absolute top-0 w-full z-10 h-14 flex justify-between items-center px-4 text-primary'>
+        <MenuIcon className='w-6 h-6' />
+        <CircleHelpIcon className='w-6 h-6' />
+      </div>
       <Map
         initialViewState={{
           longitude: 32.778,
-          latitude: 39.89,
+          latitude: 39.8915,
           zoom: 13.09,
         }}
         style={{ height: '100vh' }}
@@ -49,6 +54,16 @@ export default function Home() {
           <BusPin key={point.id} point={point} />
         ))}
       </Map>
+      <div className='absolute bottom-12 left-1/2 -translate-x-1/2 w-[90%] md:w-96 h-28'>
+        <div className='h-14 bg-primary rounded-t-xl flex justify-between items-center px-4 text-white'>
+          <span className='font-medium'>Sarı-Kırmızı Ring</span>
+          <span className='font-medium'>Kalkış 15.00</span>
+        </div>
+        <div className='h-14 bg-white rounded-b-xl flex justify-between items-center px-4 shadow-xl'>
+          <span className='font-medium'>Durak Ara</span>
+          <SearchIcon className='w-6 h-6' />
+        </div>
+      </div>
       <div className='absolute right-2 bottom-2 px-1 text-xs bg-white bg-opacity-50 rounded-full'>
         <a href='https://www.openstreetmap.org/copyright' target='_blank'>
           © OpenStreetMap
