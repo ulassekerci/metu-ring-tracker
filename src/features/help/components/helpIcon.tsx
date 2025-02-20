a397377088366443437050b873ea5bc0e394936c
ulassekerci <107992457+ulassekerci@users.noreply.github.com>
2025-02-20 23:02:34 +0300

    Help icon cursor pointer

--- src/features/help/components/helpIcon.tsx
+++ src/features/help/components/helpIcon.tsx
@@ -5,7 +5,7 @@ export const HelpIcon = () => {
   const { open } = useDrawerStore()
 
   return (
-    <div className='absolute right-2 top-4 rounded-md bg-white bg-opacity-90 h-9 w-9 drop-shadow-xl grid place-content-center'>
+    <div className='absolute right-2 top-4 rounded-md bg-white bg-opacity-90 h-9 w-9 drop-shadow-xl grid place-content-center cursor-pointer'>
       <CircleHelpIcon className='w-6 h-6 opacity-90' onClick={open} />
     </div>
   )
