44e92286affd6c75c97d20e36220529bfdcd685b
ulassekerci <107992457+ulassekerci@users.noreply.github.com>
2025-03-04 13:18:44 +0300

    Updated error margin text

--- src/features/box/data/errorMargin.ts
+++ src/features/box/data/errorMargin.ts
@@ -1,4 +1,4 @@
 export const getErrorMargin = (maxDistance?: number) => {
-  if (!maxDistance || isNaN(maxDistance)) return 'Bilinmiyor'
-  else return maxDistance.toFixed(0) + 'm'
+  if (!maxDistance || isNaN(maxDistance)) return 'Çok Yüksek'
+  else return maxDistance > 1500 ? 'Yüksek' : 'Düşük'
 }
