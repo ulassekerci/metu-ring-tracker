1fd318d888f03d59f6f7460eb18dfeeab7f34d0f
ulassekerci <107992457+ulassekerci@users.noreply.github.com>
2025-09-25 16:10:17 +0300

    re-implement schedule

--- src/features/box/components/InfoBox.tsx
+++ src/features/box/components/InfoBox.tsx
@@ -2,16 +2,16 @@ import { motion } from 'motion/react'
 import { useInfoBoxStore } from '../store'
 import { cn } from '@/utils/cn'
 import { BusDisplay, GhostDisplay } from './PinData'
+import { getNextTrip, getRingNameFromColor, useSchedule } from '../data/schedule'
 
 export const InfoBox = () => {
-  // const { data: schedule } = useSchedule()
+  const { data: schedule } = useSchedule()
   const { selected } = useInfoBoxStore()
 
-  // if (!schedule) return null
-  // const nextTrip = getNextTrip(schedule)
+  if (!schedule) return null
+  const nextTrip = getNextTrip(schedule)
   // TODO: Try displaying multiple colors (19.00 and 20.00 rings)
-  // const nextTripColor = getRingNameFromColor(nextTrip.colors[0])
-  // const nextTripTime = DateTime.fromFormat(nextTrip.time, 'HH:mm:ss').toFormat('HH.mm')
+  const nextTripColor = getRingNameFromColor(nextTrip.colors[0])
 
   return (
     <div className='absolute bottom-12 left-1/2 -translate-x-1/2 w-[90%] md:w-96 shadow-xl'>
@@ -21,15 +21,15 @@ export const InfoBox = () => {
         className={cn('rounded-xl', selected ? 'bg-white' : 'bg-primary text-white')}
       >
         <div>
-          {/* {!selected && (
+          {!selected && (
             <div className='flex items-center justify-between w-full h-14 px-4'>
               <div className='flex items-center'>
                 <span className='font-semibold mr-1'>Sıradaki:</span>
                 <span className='font-medium'>{nextTripColor}</span>
               </div>
-              <span className='font-medium'>{nextTripTime}</span>
+              <span className='font-medium'>{nextTrip.time}</span>
             </div>
-          )} */}
+          )}
           {selected === 'ghost' && <GhostDisplay />}
           {selected === 'bus' && <BusDisplay />}
         </div>
