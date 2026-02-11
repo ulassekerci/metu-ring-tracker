7301b58eee94e9253e314b4675caf22301df74e9
ulassekerci <107992457+ulassekerci@users.noreply.github.com>
2026-02-12 01:57:59 +0300

    remove dashboard

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,24 +1,7 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Home from '@/app/routes/home'
-import Trips from '@/app/routes/dash/trips/trips'
-import Trip from '@/app/routes/dash/trips/trip'
-import { Login } from './routes/login'
-import Stats from './routes/dash/stats'
-import Dashboard from './routes/dash'
 
-const router = createBrowserRouter([
-  { path: '/', element: <Home /> },
-  { path: '/login', element: <Login /> },
-  {
-    path: '/dashboard',
-    element: <Dashboard />,
-    children: [
-      { path: '', element: <Stats /> },
-      { path: 'trips', element: <Trips /> },
-      { path: 'trips/:tripID', element: <Trip /> },
-    ],
-  },
-])
+const router = createBrowserRouter([{ path: '/', element: <Home /> }])
 
 export const AppRouter = () => {
   return <RouterProvider router={router} />
