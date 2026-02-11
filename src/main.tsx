e46b175b9e4677346d9869c5496f013ae3ad0d8e
ulassekerci <107992457+ulassekerci@users.noreply.github.com>
2026-02-12 01:58:32 +0300

    set default timezone

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import { App } from '@/app'
 import '@/tailwind.css'
 import 'maplibre-gl/dist/maplibre-gl.css'
+import { Settings } from 'luxon'
+
+Settings.defaultZone = 'Europe/Istanbul'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
 )
